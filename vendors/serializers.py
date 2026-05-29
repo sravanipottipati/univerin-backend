54d22a011b9acad570c52284f2c09e3d1eebb2ea
Sravani Polamreddy <sravlaks18112015@gmail.com>
2026-05-29 11:52:19 -0400

    Fix 4C: bank fields in serializer and allowed_fields

--- vendors/serializers.py
+++ vendors/serializers.py
@@ -50,7 +50,8 @@ class VendorSerializer(serializers.ModelSerializer):
                   'delivery_radius',                          # ← NEW
                   'rating', 'total_reviews', 'platform_fee',
                   'is_open', 'status', 'products',
-                  'distance', 'created_at']
+                  'distance', 'created_at',
+                  'bank_account_name', 'bank_account_number', 'bank_ifsc_code', 'bank_name']
 
     def get_distance(self, obj):
         request = self.context.get('request')
